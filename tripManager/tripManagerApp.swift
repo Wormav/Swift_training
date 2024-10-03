b05934e4ee0c975c935228ccb20e7a0900978bd5
Jérémy Lorette <jeremy.lorette@outlook.com>
2024-10-03 21:18:46 +0200

    Initial Commit

---
+++ tripManager/tripManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  tripManagerApp.swift
+//  tripManager
+//
+//  Created by Jérémy Lorette on 03/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct tripManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
